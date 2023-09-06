cfb0e6d1bcf09d5123cb371ef0c58e25171e71c4
PetrT <140140070+196-PetrT@users.noreply.github.com>
2023-09-06 21:19:11 +0300

    add task 46 and task 51

---
+++ mainDiagonal/Program.cs
@@ -0,0 +1,53 @@
+﻿// Задача 51: Задайте двумерный массив. Найдите сумму элементов, находящихся на главной диагонали 
+// (с индексами (0,0); (1;1) и т.д. Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Сумма элементов главной диагонали: 1+9+2 = 12
+
+
+int GetSumMainDiagonale(int[,] matrix)
+{
+    int sum = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (i == j) sum += matrix[i,j];
+        }
+    }
+    return sum;
+}
+
+int[,] GetMatrix(int rows = 4, int columns = 4)
+{
+    Random rand = new Random();
+    int[,] matrix = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            matrix[i, j] = rand.Next(0,10);
+        }
+    }
+    return matrix;
+
+}
+
+void PrintMatrix(int[,] matrix) 
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] mainMatrix = GetMatrix();
+PrintMatrix(mainMatrix);
+// int sum = GetSum(mainMatrix);
+// System.Console.WriteLine($"Сумма элементов главной диагонали = {sum}"); или можно написать
+System.Console.WriteLine($"Сумма элементов главной диагонали = {GetSumMainDiagonale(mainMatrix)}");
---
+++ teacherCode/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задача 46: Задайте двумерный массив размером m x n, 
+// заполненный случайными целыми числами
+// m = 3, n = 4.
+
+
+int[,] GetMatrix(int rows, int columns, int minValue = 10, int maxValue = 99)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rand = new Random();
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            matrix[i, j] = rand.Next(minValue, maxValue+1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix) // метод вывода массива на экран
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int SetNumber(string message = " ") // добавим метод для получения чисел вводом с консоли
+{
+    System.Console.WriteLine($"Enter number {message} : ");
+    return Convert.ToInt32(Console.ReadLine());
+    // или
+    // int num = Convert.ToInt32(Console.ReadLine());
+    // return num;
+}
+
+int rows1 = SetNumber("rows");
+int columns1 = SetNumber("columns");
+int maxValue1 = SetNumber("maxValue");
+int minValue1 = SetNumber("minValue");
+
+// int [,] matrix = GetMatrix(rows, columns); - чтобы можно было передавать в другом порядке
+int [,] matrix = GetMatrix(rows: rows1, columns: columns1);
+PrintMatrix(matrix);
